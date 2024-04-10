b529b515bd7d8f18f80fa646c1c70f630e9f41bf
benraid <137850850+benraid@users.noreply.github.com>
2024-04-10 17:07:12 -0400

    Update views.py
    register/login views and some updates to others

--- HealthTracker/views.py
+++ HealthTracker/views.py
@@ -2,6 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .forms import RegisterForm
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            # Create a new user instance
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(username=username, email=email, password=password)
+            user.save()
+            # Redirect to a success page or login page
+            return redirect('registration/login.html')
+    else:
+        form = RegisterForm()
+    return render(request, 'registration/register.html', {'form': form})
 
 def index(request):
     template = loader.get_template("HealthTracker/index.html")
@@ -10,3 +29,17 @@ def index(request):
 
 def about(request):
     return render(request, 'HealthTracker/about.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index.html')
+        else:
+            error_message = 'Invalid credentials'
+            return render(request, 'registration/login.html', {'error_message': error_message})
+    else:
+        return render(request, 'registration/login.html')
